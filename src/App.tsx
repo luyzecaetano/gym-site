3a66256df8a1e638430f74cce3ae354f7020c348
luyzecaetano <luyzecaetano@gmail.com>
2025-06-20 13:39:40 -0300

    criação da home, adicionado bg para navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import Navbar from "@/scenes/navbar/navbar";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
+import Home from "./scenes/home/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
   );
 
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
