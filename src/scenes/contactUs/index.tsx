35059215707d2eb56996b7dc57664e36a5ff8a02
luyze <168435824+luyzecaetano@users.noreply.github.com>
2025-06-25 22:33:49 -0300

    ContactUs e Criação do footer

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -4,16 +4,13 @@ import HText from "@/shared/HText.tsx";
 import { useForm } from "react-hook-form";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 
-ContactUsPageGraphic;
-useForm();
-motion;
-HText;
-
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
   const {
     register,
     trigger,
@@ -49,12 +46,10 @@ const ContactUs = ({ setSelectedPage }: Props) => {
           </HText>
 
           <p className="my-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-            asperiores at aut, blanditiis, cupiditate ducimus hic illum
-            inventore ipsam ipsum laborum laudantium nam, natus nulla omnis
-            optio perferendis quibusdam sint veniam veritatis. Esse itaque iure
-            pariatur vero voluptatibus? Exercitationem illo magnam minima nam
-            sint tempore temporibus?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+            dolores enim eum, eveniet fugit hic impedit magnam nihil placeat
+            quaerat repellat veniam, vitae. Dolores quia repellendus similique
+            tempore?
           </p>
         </motion.div>
 
@@ -71,7 +66,79 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               visible: { opacity: 1, x: 0 },
             }}
           >
-            <form target="_blank" onSubmit={onSubmit} method="POST"></form>
+            <form target="_blank" onSubmit={onSubmit} action="" method="POST">
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="NAME"
+                {...register("name", { required: true, maxLength: 100 })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                {...register("message", { required: true, maxLength: 2000 })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 characters."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
+            </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: 50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full"
+                src={ContactUsPageGraphic}
+                alt="ContactUsPageGraphic"
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,33 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet
+            architecto, aspernatur, consectetur consequatur expedita harum illum
+            iusto maiores maxime minus non possimus quaerat quidem voluptate.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+        <div className="basis-1/8 mt-16 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+
+          <p>(333) 425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
